143d040d02f31c729c2a3416e4ee34ba3f53b868
manishkm <manishkm@dewsolutions.in>
2017-08-05 18:35:07 +0530

    changes in import

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,9 @@
-var app = angular.module('app', ['NVConfig','ui.router','duScroll','ngMaterial']);
+  var app = angular.module('app', ['NVConfig', 'ui.router', 'duScroll', 'ngMaterial',
+    'ngResource',
+    'ngStorage', 'angular.filter',
+  ]);
 
-app.config(function ($mdThemingProvider) {
+  app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('red')
       .primaryPalette('red');
 
