d08ef22b4a85cbeabb9cadd93af0e7265e1540c7
mukeshs12 <mukeshs@dewsolutions.in>
2017-08-14 16:26:34 +0530

    partner form submit

--- public/app/components/partner/partnerController.js
+++ public/app/components/partner/partnerController.js
@@ -1,25 +1,32 @@
-/*app
-  .controller("partnerController",
-  function ($scope, $http, $rootScope) {
+'use strict';
+app.controller('partnerController', function ($scope, $http, $rootScope, RootAPIServices,$mdDialog, $timeout) {
+   $scope.partner = {};
+   $scope.response = {"isSuccess": false};
 
-    $rootScope.active = "become_partner";
 
-    console.log('---', $rootScope.active);
+  $scope.becomePartner = function (ev) {
+    var body = $scope.partner;
+    console.log("submit click", body);
+    RootAPIServices.rootApi.becomePartnerService({}, body).$promise.then(function (response) {
+      console.log("result===", response);
+      $scope.response.isSuccess = true;
+      $scope.partner = {};
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Thank you!')
+          .textContent('Your request has been submitted.Our team will get back to you shortly.')
+         // .htmlContent('Your request has been submitted.<br>Our team will get back to you shortly.')
+          .ariaLabel('')
+          .ok('Ok')
+          .targetEvent(ev)
+      );
+
+      $scope.partnerForm.$setPristine();
+      $scope.partnerForm.$setUntouched();
+   
+    });
   }
-  )*/
 
-/*app.module('myApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages'])*/
-
-app.controller('partnerController', function ($scope, $http, $rootScope) {
-    $scope.submit = function () {
-      // submit code goes here
-    };
-    $scope.reset = function () {
-      /* $scope.obj = {
-         name: "",
-         myselect: "",
-         status: ""
-       }*/
-    }
-    $scope.reset();
-  });
+});
