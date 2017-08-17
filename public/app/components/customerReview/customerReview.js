044d5d9c56d6ca0f648f41f18fc57e90d592e506
manishkm <manishkm@dewsolutions.in>
2017-08-17 21:57:57 +0530

    new commit

--- public/app/components/customerReview/customerReview.js
+++ public/app/components/customerReview/customerReview.js
@@ -5,7 +5,7 @@ app.config(['slickCarouselConfig', function (slickCarouselConfig) {
   .controller("customertestinomial",
   function ($scope, $http, $rootScope, $timeout) {
 
-   $scope.number4 = [{label: 225}, {label: 125}, {label: 200}, {label: 175}, {label: 150}, {label: 180}, {label: 300}, {label: 400}];
+   $scope.number4 = [{label: 200}, {label: 200}, {label: 200}, {label: 200}, {label: 200}, {label: 200}, {label: 200}, {label: 200}];
     $scope.slickConfig4Loaded = true;
     $scope.updateNumber4 = function () {
       $scope.slickConfig4Loaded = false;
