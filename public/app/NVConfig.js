df45b8d43ce178f5c2226f65553d48ec6f8c0c3d
manishkm <manishkm@dewsolutions.in>
2017-08-26 20:35:04 +0530

    new changes

--- public/app/NVConfig.js
+++ public/app/NVConfig.js
@@ -2,6 +2,7 @@ angular.module('NVConfig', [])
 
 .constant('NVConfig', {
 	webservice_domain : 'http://localhost:9090/NovoWash',
+	// webservice_domain : 'http://52.32.68.31:8080/NovoWash',
 	})
 
 ;
