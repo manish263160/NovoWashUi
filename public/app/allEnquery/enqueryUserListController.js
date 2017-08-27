096638ae40efe74b8fc8f01f9d4d2d4247fbec53
manishkm <manishkm@dewsolutions.in>
2017-08-27 23:07:47 +0530

    new files

--- public/app/allEnquery/enqueryUserListController.js
+++ public/app/allEnquery/enqueryUserListController.js
@@ -11,5 +11,16 @@ function ($scope, $http, $rootScope, $filter, ngTableParams, $document, RootAPIS
     
     {"id":50,"first_name":"Andrea","last_name":"Greene","email":"agreene4@fda.gov","country":"Russia","ip_address":"128.72.13.52"}];
 
+
+    $scope.usersTable = new ngTableParams({
+        page: 1,
+        count: 10
+    }, {
+        total: $scope.users.length, 
+        getData: function ($defer, params) {
+            $scope.data = $scope.users.slice((params.page() - 1) * params.count(), params.page() * params.count());
+            $defer.resolve($scope.data);
+        }
+    });
 });
 
