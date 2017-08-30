35e497858d455d6dae4d2de66471674a8ad6221e
manishkm <manishkm@dewsolutions.in>
2017-08-30 23:24:41 +0530

    add image

--- public/app/NVConfig.js
+++ public/app/NVConfig.js
@@ -1,7 +1,7 @@
 angular.module('NVConfig', [])
 
 .constant('NVConfig', {
-	webservice_domain : 'http://localhost:8080/NovoWash',
+	webservice_domain : 'http://52.32.68.31:8080/NovoWash',
 	// webservice_domain : 'http://52.32.68.31:8080/NovoWash',
 	})
 
